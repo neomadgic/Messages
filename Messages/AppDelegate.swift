4d4d0c7e92b91363c0af67c1b00dc3b65067c2af
Vu Dang <jublusion5@gmail.com>
2017-05-21 13:58:46 -0500

    Successfully loaded MessageViewController

--- Messages/AppDelegate.swift
+++ Messages/AppDelegate.swift
@@ -17,6 +17,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
+        let homeViewController = MessageViewController()
+        if window != nil {
+            window!.rootViewController = homeViewController
+            window!.makeKeyAndVisible()
+        } else {
+            print("Error loading homeViewController")
+        }
         return true
     }
 
