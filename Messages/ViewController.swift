40a6151590f0e67594d3307828f2ef1699d9f481
Neomadgic <jublusion5@gmail.com>
2017-05-21 13:29:12 -0500

    Initial Commit

---
+++ Messages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Messages
+//
+//  Created by Vu Dang on 5/21/17.
+//  Copyright © 2017 Vu Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
