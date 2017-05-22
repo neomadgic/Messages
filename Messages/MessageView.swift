f30fb2165696df58d848ee416d513342ee435a62
Vu Dang <jublusion5@gmail.com>
2017-05-22 18:43:08 -0500

    Added functions to move the view up when keyboard appears

--- Messages/MessageView.swift
+++ Messages/MessageView.swift
@@ -9,21 +9,29 @@
 import UIKit
 
 class MessageView: UIView {
+    
+    let messageTextField = MessageTextField(frame: CGRect(x: 20.0, y: 100.0, width: 300.0, height: 20.0))
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let messageTextField = MessageTextField(frame: CGRect(x: 20.0, y: 100.0, width: 300.0, height: 20.0))
-        addSubview(messageTextField)
-        messageTextField.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -4.0).isActive = true
-        messageTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8.0).isActive = true
-        messageTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8.0).isActive = true
-        messageTextField.topAnchor.constraint(equalTo: self.topAnchor, constant: 4.0).isActive = true
-        
         translatesAutoresizingMaskIntoConstraints = false
+        addMessageTextField()
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    func addMessageTextField() {
+        
+        addSubview(messageTextField)
+        
+        //Create messageTextField Constraints
+        messageTextField.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -4.0).isActive = true
+        messageTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8.0).isActive = true
+        messageTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8.0).isActive = true
+        messageTextField.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
+    }
+    
 
 }
--- Messages/MessageViewController.swift
+++ Messages/MessageViewController.swift
@@ -10,19 +10,43 @@ import UIKit
 
 class MessageViewController: UIViewController {
 
+    let messageView = MessageView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor.cyan
+        createMessageView()
+        NotificationCenter.default.addObserver(self, selector: #selector(MessageViewController.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(MessageViewController.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
         
-        self.view.backgroundColor = UIColor.red
-        let messageView = MessageView()
-        messageView.backgroundColor = UIColor.green
-        messageView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    func createMessageView() {
+        
         self.view.addSubview(messageView)
         messageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
         messageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
         messageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
-        messageView.heightAnchor.constraint(equalToConstant: 40.0).isActive = true
-    
+        messageView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
     }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y != 0{
+                self.view.frame.origin.y += keyboardSize.height
+            }
+        }
+    }
+    
 }
